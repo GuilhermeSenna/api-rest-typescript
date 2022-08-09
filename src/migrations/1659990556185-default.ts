214ab70f5c37082b68147e95ebf36d8819bcbc22
zBiggui <bigguisenna@gmail.com>
2022-08-08 22:50:17 -0300

    feat: add newest migration (add 'description' column on rooms table

--- src/migrations/1659990556185-default.ts
+++ src/migrations/1659990556185-default.ts
@@ -8,7 +8,7 @@ export class default1659990556185 implements MigrationInterface {
         await queryRunner.query(`CREATE TABLE "room_subject" ("room_id" integer NOT NULL, "subject_id" integer NOT NULL, CONSTRAINT "PK_6b3738a7b93c77fd6d9333b638a" PRIMARY KEY ("room_id", "subject_id"))`);
         await queryRunner.query(`CREATE INDEX "IDX_f227421d2ef64ab086261ac07f" ON "room_subject" ("room_id") `);
         await queryRunner.query(`CREATE INDEX "IDX_a05f10c497f5f7db3022664a6d" ON "room_subject" ("subject_id") `);
-        await queryRunner.query(`ALTER TABLE "rooms" ADD "descript" text`);
+        await queryRunner.query(`ALTER TABLE "rooms" ADD "description" text`);
         await queryRunner.query(`ALTER TABLE "room_subject" ADD CONSTRAINT "FK_f227421d2ef64ab086261ac07fd" FOREIGN KEY ("room_id") REFERENCES "subjects"("id") ON DELETE CASCADE ON UPDATE CASCADE`);
         await queryRunner.query(`ALTER TABLE "room_subject" ADD CONSTRAINT "FK_a05f10c497f5f7db3022664a6d6" FOREIGN KEY ("subject_id") REFERENCES "rooms"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
     }
