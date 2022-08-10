042ea4255a9d0b374035fe50381c3b5e24c656c3
zBiggui <bigguisenna@gmail.com>
2022-08-09 22:37:07 -0300

    refactor: change server initialization to use with supertest and jest

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,9 @@
 import express from 'express';
 import { AppDataSource } from './data-source';
-import routes from './routes';
+import { createServer } from './util/server'
+
+const app = createServer()
 
 AppDataSource.initialize().then(() => {
-    const app = express();
-
-    app.use(express.json());
-
-    app.use(routes);
-
     return app.listen(process.env.port);
 });
