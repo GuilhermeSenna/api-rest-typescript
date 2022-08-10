524b05fcc222cc94ff1965422df1412e86ef9e61
zBiggui <bigguisenna@gmail.com>
2022-08-10 13:23:48 -0300

    test: add test for the route

--- src/__tests__/subject.test.ts
+++ src/__tests__/subject.test.ts
@@ -1,30 +1,49 @@
+jest.useFakeTimers()
+
 import supertest from 'supertest'
 import { createServer } from '../util/server'
 import { AppDataSource } from '../data-source';
+import { DataSource } from 'typeorm';
 
-jest.useFakeTimers();
+let app: any;
 
-const app = createServer();
+beforeAll(async () => {
+    app = createServer();
 
+    AppDataSource.initialize();
+});
 
 
 describe('subject', () => {
-
-    beforeAll(async () => {
-        // AppDataSource.initialize().then(() => {
-        //     return app.listen(process.env.port);
-        // });
-
-        AppDataSource.initialize();
-    });
-
     describe('get subject route', () => {
-        describe('given the subject does not exist', () => {
-            it("should return a 404", async () => {
-                const subjectId = 0;
 
-                const res = await supertest(app).get(`/subject/0`);
-            });
+        it("should return 404 to an non-existent user", async () => {
+            const query = { userShouldExist: false }
+
+            const res = await supertest(app)
+                .post('/testsubject')
+                .send(query)
+                .expect(404);
+        });
+
+        it("should return 201 to successfully create a new user", async () => {
+            const name = 'fulano';
+            const query = { userShouldExist: true, name }
+
+            const res = await supertest(app)
+                .post('/testsubject')
+                .send(query)
+                .expect({ name }) // Return the name of created user
+                .expect(201);
+        });
+
+        describe('given the subject does not exist', () => {
+
+            // it("should return a 404", async () => {
+            //     const subjectId = 0;
+
+            //     const res = await supertest(app).get(`/subject/0`).expect(404);
+            // });
         });
     });
 });
