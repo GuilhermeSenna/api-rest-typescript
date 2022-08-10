4ae1904076a50845c50abc970d4b1eb1da816d79
zBiggui <bigguisenna@gmail.com>
2022-08-10 20:45:25 -0300

    refactor: better organization for index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 import express from 'express';
-import { AppDataSource } from './data-source';
+import { InMemoryDatabase } from './data-source';
 import { createServer } from './util/server'
 
 const app = createServer()
 
-AppDataSource.initialize().then(() => {
-    return app.listen(process.env.port);
-});
+const startServer = async () => {
+    await InMemoryDatabase.initialize();
+    console.log('Connected to the database!');
+
+    app.listen(process.env.port);
+    console.log('Server running on port:', process.env.port);
+}
+
+startServer();
