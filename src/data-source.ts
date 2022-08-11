e53ad12d6baa3c3c3cde8c09e512e52261cda7e7
zBiggui <bigguisenna@gmail.com>
2022-08-11 13:31:26 -0300

    refactor: start specific database based on STATUS in .env

--- src/data-source.ts
+++ src/data-source.ts
@@ -4,22 +4,28 @@ import { DataSource } from "typeorm";
 
 const port = process.env.DB_PORT as number | undefined;
 
-export const AppDataSource = new DataSource({
-    type: 'postgres',
-    host: process.env.DB_HOST,
-    port: port,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    entities: [`${__dirname}/**/entities/*.{ts,js}`],
-    migrations: [`${__dirname}/**/migrations/*.{ts,js}`],
-    migrationsRun: true, // automatically run migrations before the tests
-});
+export let AppDataSource: DataSource;
 
-export const InMemoryDatabase = new DataSource({
-    name: 'default',
-    type: 'better-sqlite3',
-    database: ':memory:',
-    entities: [`${__dirname}/**/entities/*.{ts,js}`],
-    synchronize: true
-});
+if (process.env.STATUS === 'test') {
+    AppDataSource = new DataSource({
+        name: 'default',
+        type: 'better-sqlite3',
+        database: ':memory:',
+        entities: [`${__dirname}/**/entities/*.{ts,js}`],
+        migrations: [`${__dirname}/**/migrations/testmigrations/*.{ts,js}`],
+        // synchronize: true,
+        migrationsRun: true
+    });
+} else {
+    AppDataSource = new DataSource({
+        type: 'postgres',
+        host: process.env.DB_HOST,
+        port: port,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
+        entities: [`${__dirname}/**/entities/*.{ts,js}`],
+        migrations: [`${__dirname}/**/migrations/development/*.{ts,js}`],
+        // migrationsRun: true, // automatically run migrations before the tests
+    });
+}
